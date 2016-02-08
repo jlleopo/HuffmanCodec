49126afccde5c2b34e56f85848505f9daf4e9292
Jesse Leopold <mr.shalaka@gmail.com>
2016-02-08 17:10:21 -0500

    gathered symbols from file

--- Huffcodec.c
+++ Huffcodec.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BIGNUM 1000
+
+typedef struct node{
+  char symb;
+  struct node *left;
+  struct node *right;
+} t_node;
 
 void compress(FILE *fptIn, FILE *fptOut);
 
 void decompress(FILE *fptIn, FILE *fptOut);
 
-int main(int argc, char *argv[]){
+int main(int argc, unsigned char *argv[]){
     FILE *fptIn;
     FILE *fptOut;
 
@@ -32,10 +39,16 @@ int main(int argc, char *argv[]){
 }
 
 void compress(FILE *fptIn, FILE *fptOut){
-    char *allSymbols;   //list of symbols found in fptIn
+    unsigned char *allSymbols;   //list of symbols found in fptIn
     int *freqs;         //frequencies of said symbols
-    //to do: do we dynamically allocate this or just pick a huge number and call it a day?
-    char c;
+    unsigned char c;
+
+    t_node *ptrees;
+    t_node ftree;
+
+    allSymbols = (unsigned char *) calloc(BIGNUM, sizeof(unsigned char));
+    freqs = (int *) calloc(BIGNUM, sizeof(int));
+
 
     //build full list of symbols
     while(fread(&c, 1, 1, fptIn) == 1){
@@ -53,10 +66,50 @@ void compress(FILE *fptIn, FILE *fptOut){
     }
 
     //build tree....
+    //sort
+    mSort(char *cs, int *freqs);
 }
 
 //returns 0 if c is not contained in list
 //return 1 if is and sets i to index
-int containedIn(char *list, char c, int *i){
+int containedIn(unsigned char *list, unsigned char c, int *i){
+    int j;
+    for(j=0; j<BIGNUM; j++){
+        if(list[j] == c){
+            *i = j;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void mSort(unsigned char *cs, int *freqs){
+    int i, j, k;
+    int len=0;
+    int swapped;
+    char ctemp;
+    int itemp;
+
+    while(freqs[len]!=0) len++;
+
+    do{
+        swapped = 0;
+        for(i=1; i<lens; i++){
+            if(freqs[i-1] > freqs[i]){
+                //swap chars
+                ctemp=cs[i];
+                cs[i]=cs[i-1];
+                cs[i-1]=ctemp;
+
+                //swap freqs
+                itemp=freqs[i];
+                freqs[i]=freqs[i-1];
+                freqs[i-1]=itemp;
+
+                swapped = 1;
+            }
+        }
+        len--;
+    }while(swapped==1);
 
 }
