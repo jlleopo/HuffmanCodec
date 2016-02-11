ebf0cb9e0704f3e046be523336043f011c3530a5
Jesse Leopold <mr.shalaka@gmail.com>
2016-02-10 20:52:26 -0500

    added output function

--- Huffcodec.c
+++ Huffcodec.c
@@ -15,6 +15,9 @@ void compress(FILE *fptIn, FILE *fptOut);
 int containedIn(unsigned char *list, unsigned char c, int *i);
 void mSort(unsigned char *cs, int *freqs);
 void sortTrees(t_node **trees, int len);
+void formCodes(t_node *node, char *currentCode, char **codeList, unsigned char *symbolList, int *i);
+void writeOutCodes(FILE *fptIn, FILE *fptOut, char **codeList, int numCodes, unsigned char *symbolList);
+unsigned char makeBitmask(int length);
 
 
 void decompress(FILE *fptIn, FILE *fptOut);
@@ -49,6 +52,9 @@ void compress(FILE *fptIn, FILE *fptOut){
     unsigned char c;
     int i;
     int len;
+    char *currentCode;
+    char **codeList;
+    unsigned char *symbolList;
 
     t_node **ptrees;
     t_node *newNode;
@@ -104,6 +110,14 @@ void compress(FILE *fptIn, FILE *fptOut){
         //sort
         sortTrees(ptrees, len);
     }
+
+    codeList = (char **) calloc(len, sizeof(char *));
+    currentCode = (char *) calloc(100, sizeof(char));
+    symbolList = (unsigned char *) calloc(len, sizeof(unsigned char));
+    i=0;
+    formCodes(ptrees[0], currentCode, codeList, symbolList, &i);
+
+    writeOutCodes(fptIn, fptOut, codeList, len, symbolList);
 
 }
 
@@ -179,6 +193,84 @@ void sortTrees(t_node **trees, int len){
 
 }
 
+//if we go left add a 0
+//if we go right add a 1
+//
+//I don't think there's any way we could have a node on only one side
+void formCodes(t_node *node, char *currentCode, char **codeList, unsigned char *symbolList, int *i){
+    if(node->left == NULL && node->right == NULL){
+        codeList[*i] = (char *) malloc(sizeof(char)*100);
+        strcpy(codeList[*i], currentCode);
+        symbolList[*i]=node->symb;
+        (*i)++;
+    } else {
+        //add a 1
+        int len = strlen(currentCode);
+        currentCode[len] = '1';
+        currentCode[len+1] = 0x0;
+        //go right
+        formCodes(node->right, currentCode, codeList, symbolList, i);
+
+        //add a 0
+        currentCode[len] = '0';
+        currentCode[len+1] = 0x0;
+        //go left
+        formCodes(node->left, currentCode, codeList, symbolList, i);
+    }
+}
+
+void writeOutCodes(FILE *fptIn, FILE *fptOut, char **codeList, int numCodes, unsigned char *symbolList){
+    int *lengths;
+    int i;
+    int position;
+    unsigned char c; //input byte
+    unsigned char b; //output byte
+    unsigned char mask;
+    unsigned char *bCodeList;
+
+    lengths = (int *) calloc(numCodes, sizeof(int));
+    bCodeList = (unsigned char *) calloc(numCodes, sizeof(unsigned char));
+    for(i=0; i<numCodes; i++){
+        lengths[i] = strlen(codeList[i]);
+        bCodeList[i] = (char) strtol(codeList[i], NULL, 2);
+    }
+
+    position = 8;
+    while(fread(&c, 1, 1, fptIn) == 1){
+        for(i=0; i<numCodes; i++){
+            if(symbolList[i]==c) break;
+        }
+
+        //make bitmask lengths[i] long
+        mask = makeBitmask(lengths[i]); // might not need this  yolo
+
+        if(position > lengths[i]){
+            //we have enough space to fit all of our pattern in the output byte
+            b |= (bCodeList[i] << position);
+            position -= lengths[i];
+        } else {
+            b |= (bCodeList[i] >> (position - lengths[i]));
+            fwrite(&b, 1, 1, fptOut);
+            b = 0x0;
+
+            b |= (bCodeList[i] >> (8 - lengths[i] + position));
+            position = 8 - lengths[i] + position;
+        }
+
+
+
+    }
+}
+
+unsigned char makeBitmask(int length){
+    int i;
+    char c=0;
+
+    for(i=0; i<length; i++){
+        c &= 1<<i;
+    }
+    return c;
+}
 
 void decompress(FILE *fptIn, FILE *fptOut){
     int i;
